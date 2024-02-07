ece22598cb9e81c570cd6375e00b9d122da1ac77
Fadhli Almunawar <64172550+momos1337@users.noreply.github.com>
2024-02-07 10:18:28 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+    var maxnumhours = 23;
+    var maxnummins = 59;
+    var maxnumsecs = 60;
+    var maxmilisecs = 999;
+    
+    
+    function updateNumbers() {
+    var start_time = 1681493552000;
+    var duration = parseInt(Date.now() / 1000 - start_time);
+    
+    var seconds = duration % 60;
+    if (seconds < 10)
+    seconds = "0" + seconds;
+    duration = parseInt(duration / 60);
+    var minutes = duration % 60;
+    if (minutes < 10)
+    minutes = "0" + minutes;
+    duration = parseInt(duration / 60);
+    var hours = duration % 24;
+    if (hours < 10)
+    hours = "0" + hours;
+    duration = parseInt(duration / 24);
+    var days = duration;
+    $('.days-number').text("" + days);
+    $('.seconds').text(hours + " hours " + minutes + " minutes " + seconds + " seconds");
+    setTimeout("updateNumbers()", 1000);
+    }
+    </script>
+    <script type="text/javascript">
+        document.ready = function() {
+            $(window).resize();
+            updateNumbers();
+        };
